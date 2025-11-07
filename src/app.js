629d7aa5a369ae987f4c967306c669ed8c9a6217
ORDAZ <cordaz@corsusa.com>
2025-11-07 11:31:45 -0500

    xx

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 require('./models');
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','https://inventario-frontend-seven.vercel.app'],
     credentials: true, 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
